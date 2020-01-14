12437c1fd1a4a30588d02665c7a335cda1c8b9f3
ryan.herrmann <ryan.herrmann@mu-c1-17.mullen.pvt.k12.co.us>
2020-01-14 11:18:33 -0700

    Created file reader clas as well as the pojo for the student

--- SISMaster/src/Student.java
+++ SISMaster/src/Student.java
@@ -1,5 +1,141 @@
 
 public class Student
 	{
+		
+		private String firstName;
+		private String lastName;
+		private String firstClass;
+		private String firstGrade;
+		private String secondClass;
+		private String secondGrade;
+		private String thirdClass;
+		private String thirdGrade;
+		private double gradePointAverage;
+		
+		
+		public Student(String fN, String lN, String fC ,String fG, String sC, String sG, String tC, String tG, double gpa)
+		{
+			firstName = fN;
+			lastName = lN;
+			
+			firstClass = fC;
+			secondClass = sC;
+			thirdClass = tC;
+			
+			firstGrade = fG;
+			secondGrade = sG;
+			thirdGrade = tG;
+			
+			gradePointAverage = gpa;
+			
+		}
 
+
+		public String getFirstGrade()
+			{
+				return firstGrade;
+			}
+
+
+		public void setFirstGrade(String firstGrade)
+			{
+				this.firstGrade = firstGrade;
+			}
+
+
+		public String getSecondGrade()
+			{
+				return secondGrade;
+			}
+
+
+		public void setSecondGrade(String secondGrade)
+			{
+				this.secondGrade = secondGrade;
+			}
+
+
+		public String getThirdGrade()
+			{
+				return thirdGrade;
+			}
+
+
+		public void setThirdGrade(String thirdGrade)
+			{
+				this.thirdGrade = thirdGrade;
+			}
+
+
+		public String getFirstName()
+			{
+				return firstName;
+			}
+
+
+		public void setFirstName(String firstName)
+			{
+				this.firstName = firstName;
+			}
+
+
+		public String getLastName()
+			{
+				return lastName;
+			}
+
+
+		public void setLastName(String lastName)
+			{
+				this.lastName = lastName;
+			}
+
+
+		public String getFirstClass()
+			{
+				return firstClass;
+			}
+
+
+		public void setFirstClass(String firstClass)
+			{
+				this.firstClass = firstClass;
+			}
+
+
+		public String getSecondClass()
+			{
+				return secondClass;
+			}
+
+
+		public void setSecondClass(String secondClass)
+			{
+				this.secondClass = secondClass;
+			}
+
+
+		public String getThirdClass()
+			{
+				return thirdClass;
+			}
+
+
+		public void setThirdClass(String thirdClass)
+			{
+				this.thirdClass = thirdClass;
+			}
+
+
+		public double getGradePointAverage()
+			{
+				return gradePointAverage;
+			}
+
+
+		public void setGradePointAverage(double gradePointAverage)
+			{
+				this.gradePointAverage = gradePointAverage;
+			}
+		
 	}

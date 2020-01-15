e0f630536a7a763d1c088d4111c1fd0e847e30f6
ryan.herrmann <ryan.herrmann@mu-c1-17.mullen.pvt.k12.co.us>
2020-01-15 11:57:54 -0700

    GPA Calculator almost working

---
+++ SISMaster/src/GPACalculator.java
@@ -0,0 +1,57 @@
+import java.text.DecimalFormat;
+
+public class GPACalculator
+	{
+		
+		public static void calculateGPAForEachStudent()
+		{
+			for (int i = 0; i < SISRunner.roster.size(); i ++)
+				{
+					double firstGrade = getGrade(SISRunner.roster.get(i).getFirstGrade());
+					double secondGrade = getGrade(SISRunner.roster.get(i).getSecondGrade());
+					double thirdGrade = getGrade(SISRunner.roster.get(i).getThirdGrade());
+					
+					DecimalFormat decFor = new DecimalFormat("0.00");
+					
+					double GPAMath = (firstGrade + secondGrade + thirdGrade)/3.0;
+					String totalGPA = decFor.format(GPAMath);
+					
+					System.out.println(totalGPA);
+					//SISRunner.roster.get(i).setGradePointAverage(Double.parseDouble(totalGPA));
+				}
+		}
+		
+		private static double getGrade (String s)
+		{
+			double tempGPAForClass = 0.0;
+			
+			if (s.substring(0, 1).contentEquals("A"))
+				tempGPAForClass = 4.0;
+			
+			else if(s.substring(0, 1).contentEquals("B"))
+				tempGPAForClass = 3.0;
+			
+			else if(s.substring(0, 1).contentEquals("C"))
+				tempGPAForClass = 2.0;
+			
+			else if(s.substring(0, 1).contentEquals("D"))
+				tempGPAForClass = 1.0;
+			
+			else if(s.substring(0, 1).contentEquals("F"))
+				tempGPAForClass = 0.0;
+			
+			//Adding or subtracting .3 for pluses and minuses for GPA
+			
+			if (s.length() < 2)
+				tempGPAForClass = tempGPAForClass + 0.0;
+			
+			else if (s.substring(1).contentEquals("-"))
+				tempGPAForClass = tempGPAForClass - 0.3;
+			
+			else if (s.substring(1).equals("+"))
+				tempGPAForClass = tempGPAForClass + 0.3;
+			
+			return tempGPAForClass;
+		}
+
+	}
--- SISMaster/src/SISRunner.java
+++ SISMaster/src/SISRunner.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 
 public class SISRunner
 	{
-
+		
+		// Please place all of the main static variables here VVVVVVV
 		static ArrayList<Student> roster = new ArrayList<Student>();
 		
 		public static void main(String[] args) throws FileNotFoundException
 			{
 				FileReader.readFile();
+				GPACalculator.calculateGPAForEachStudent();
 				//testArrayList();
 			}
 		
@@ -16,7 +18,7 @@ public class SISRunner
 			{
 				for(Student t: roster)
 					{
-						System.out.println(t.getFirstName() + " " + t.getLastName());
+						System.out.println(t.getFirstName() + " " + t.getLastName() + " " + t.getGradePointAverage());
 					}
 			}
 

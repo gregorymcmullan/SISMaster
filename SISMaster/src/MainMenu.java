13eff04e1fc1ba5af3ee33d541b55630233a3390
ryan.herrmann <ryan.herrmann@mu-c1-17.mullen.pvt.k12.co.us>
2020-01-16 10:17:28 -0700

    Added the main menu ready for other classes as well as cleaning up some things. Also added a new static int input to reference to.

---
+++ SISMaster/src/MainMenu.java
@@ -0,0 +1,30 @@
+
+public class MainMenu
+	{
+		
+		public static void printMainMenu()
+			{
+				System.out.println("What would you like to do? \n"
+						+ "1) Add or delete a student? \n"
+						+ "2) Change student grades / schedule \n"
+						+ "3) Sort Students ");
+				
+				int mainMenuChoice = SISRunner.userIntInput.nextInt();
+				
+				switch (mainMenuChoice)
+				{
+					case 1:
+						//do the adding or deleting thing here
+						break;
+					case 2:
+						//do the changing here
+						break;
+					case 3:
+						//sort the students here
+					default:
+						System.out.println("Please enter a number between 1 and 3.");
+				}
+			}
+		
+		
+	}
--- SISMaster/src/MenuChoices.java
+++ SISMaster/src/MenuChoices.java
@@ -2,64 +2,55 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Comparator;
 import java.util.Scanner;
-public class MenuChoices 
-	{
-		
-		public static void askToSort() {
-			System.out.println("\t(1) sort by last name");
-			System.out.println("\t(2) sort by GPA");
-			System.out.println("\t(3) sort by period");
-			System.out.println("\t(4) return to main menu");
-			
-			Scanner userInput = new Scanner(System.in);
-			int menuChoice= userInput.nextInt();
-			if(menuChoice == 1) {
-				Collections.sort(SISRunner.roster, new NameSorter());
-				
-			}else if(menuChoice==2) {
-				Collections.sort(SISRunner.roster, newGPASorter());
-				
-			}else if(menuChoice == 3) displayPeriodSorterMenu();
-				
-			else {
-				displayAddDeleteMenu();
-			
-			
-			}
-			SISRunner.displayData();
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 
-		}
+public class MenuChoices
+	{
+
+		public static void askToSort()
+			{
+				System.out.println("\t(1) sort by last name");
+				System.out.println("\t(2) sort by GPA");
+				System.out.println("\t(3) sort by period");
+				System.out.println("\t(4) return to main menu");
+
+				Scanner userInput = new Scanner(System.in);
+				int menuChoice = userInput.nextInt();
+				if (menuChoice == 1)
+					{
+						Collections.sort(SISRunner.roster, new NameSorter());
+
+					} else if (menuChoice == 2)
+					{
+						Collections.sort(SISRunner.roster, newGPASorter());
+
+					} else if (menuChoice == 3)
+					displayPeriodSorterMenu();
+
+				else
+					{
+						displayAddDeleteMenu();
+
+					}
+				SISRunner.displayData();
+
+			}
+
 		private static void displayAddDeleteMenu()
 			{
 				// TODO Auto-generated method stub
-				
+
 			}
+
 		private static void displayPeriodSorterMenu()
 			{
 				// TODO Auto-generated method stub
-				
+
 			}
+
 		private static Comparator newGPASorter()
 			{
 				// TODO Auto-generated method stub
 				return null;
 			}
 
-
 	}
-
-
-
-
-

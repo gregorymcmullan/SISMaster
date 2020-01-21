667507eaac13be76d65992bd2f412e7e6b84fcf5
ryan.herrmann <ryan.herrmann@mu-c1-17.mullen.pvt.k12.co.us>
2020-01-21 11:38:24 -0700

    Switching grades and classes now complete

--- SISMaster/src/MainMenu.java
+++ SISMaster/src/MainMenu.java
@@ -7,7 +7,8 @@ public class MainMenu
 				System.out.println("What would you like to do? \n"
 						+ "1) Add or delete a student? \n"
 						+ "2) Change student grades / schedule \n"
-						+ "3) Sort Students ");
+						+ "3) Sort Students \n"
+						+ "4) Exit System");
 				
 				int mainMenuChoice = SISRunner.userIntInput.nextInt();
 				
@@ -19,10 +20,15 @@ public class MainMenu
 						//do the adding or deleting thing here
 						break;
 					case 2:
-						//do the changing here
+						SISRunner.switchMenuAndgrades();
 						break;
 					case 3:
 						//sort the students here
+						break;
+					case 4:
+						System.out.println("Thank you");
+						SISRunner.stillInSystem = false;
+						break;
 					default:
 						System.out.println("Please enter a number between 1 and 3.");
 				}
--- SISMaster/src/SISRunner.java
+++ SISMaster/src/SISRunner.java
@@ -10,26 +10,66 @@ public class SISRunner
 		static ArrayList<Student> roster = new ArrayList<Student>();
 		static DecimalFormat decFor = new DecimalFormat("0.00");
 		static Scanner userIntInput = new Scanner(System.in);
+		static boolean stillInSystem = true;
 		
 		public static void main(String[] args) throws FileNotFoundException
 			{
-				FileReader.readFile();
-				GPACalculator.calculateGPAForEachStudent();
-				MainMenu.printMainMenu();
+				while(stillInSystem)
+					{
+						System.out.println();
+						FileReader.readFile();
+						GPACalculator.calculateGPAForEachStudent();
+						MainMenu.printMainMenu();
 				//testArrayList();
+				//ChangeGrades.changeGrades();
+				//GPACalculator.calculateGPAForEachStudent();
+				//testArrayList();
+				//ChangeSchedule.changeSchedule();
+				//testArrayList();
+					}
 			}
 		
 		public static void testArrayList()
 			{
+				int counter = 1;
 				for(Student t: roster)
 					{
-						System.out.println(t.getFirstName() + " " + t.getLastName() + " " + t.getGradePointAverage());
+						System.out.println(counter + ": " + t.getFirstName() + " " + t.getLastName() + " " + t.getFirstClass() + " " + t.getFirstGrade() + " " + t.getSecondClass() + " " + t.getSecondGrade() + " " +  t.getThirdClass() + " " + t.getThirdGrade() + " " + t.getGradePointAverage());
+						counter++;
 					}
 			}
 
 		public static void displayData()
 			{
 				// TODO Auto-generated method stub
+				
+			}
+
+		public static void switchMenuAndgrades()
+			{
+				System.out.println("What would you like to do?\n"
+						+ "1) Change Grades\n"
+						+ "2) Change Classes");
+				
+				int choice = userIntInput.nextInt();
+				
+				if (choice == 1)
+					{
+						System.out.println("Please choose a student below to change the grades.");
+						testArrayList();
+						ChangeGrades.changeGrades();
+						GPACalculator.calculateGPAForEachStudent();
+						System.out.println("The grades have been changed.");
+						testArrayList();
+					}
+				else if(choice == 2)
+					{
+						System.out.println("Please choose a student below to change the schedule.");
+						testArrayList();
+						ChangeSchedule.changeSchedule();
+						System.out.println("The schedules have been changed.");
+						testArrayList();
+					}
 				
 			}
 

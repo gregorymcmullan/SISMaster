1a00dd3ff26153e2c74fbad6aeefc143c81f02dd
emma.mcguire <emma.mcguire@MU-C1-05.mullen.pvt.k12.co.us>
2020-01-21 10:38:30 -0700

    submenu update

--- SISMaster/src/Submenus.java
+++ SISMaster/src/Submenus.java
@@ -9,6 +9,8 @@ public class Submenus
 		public static void addOrDelete()
 		{
 			Scanner intInput = new Scanner(System.in);
+			Scanner stringInput = new Scanner(System.in);
+
 
 			System.out.println("Would you like to (1) delete a student or (2) add a student?");
 		
@@ -16,13 +18,47 @@ public class Submenus
 			
 			if (choiceOne == 1)
 				{
+					System.out.println(SISRunner.roster.get(0).getLastName());
 					i = intInput.nextInt();
 					
-					Student.class.getClass().g;
+					
+					SISRunner.roster.remove(i);
+					System.out.println(SISRunner.roster.get(0).getLastName());
+
 				}
 			
 			if (choiceOne == 2)
 				{
+					Scanner userInput;
+
+					System.out.println("What is the new student's first name?");
+					String firstName = stringInput.nextLine();
+					
+					System.out.println("What is the new student's last name?");
+					String lastName = stringInput.nextLine();
+					
+					System.out.println("What is " + firstName + lastName + "'s first class?");
+					String firstClass = stringInput.nextLine();
+					
+					System.out.println("What is " + firstName + lastName + "'s second class?");
+					String secondClass = stringInput.nextLine();
+					
+					System.out.println("What is " + firstName + lastName + "'s third class?");
+					String thirdClass = stringInput.nextLine();
+					
+					System.out.println("What is " + firstName + "'s grade in " + firstClass + "?");
+					String firstGrade = stringInput.nextLine();
+					
+					System.out.println("What is " + firstName + "'s grade in " + secondClass + "?");
+					String secondGrade = stringInput.nextLine();
+					
+					System.out.println("What is " + firstName + "'s grade in " + thirdClass + "?");
+					String thirdGrade = stringInput.nextLine();
+					
+					System.out.println("Finally, what is " + firstName + lastName + "'s GPA?");
+					double gpa = stringInput.nextDouble();
+				
+
 					
 				}
 		
